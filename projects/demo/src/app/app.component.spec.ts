db5232b3dab667daf0bea5cb2ff8b998d0295fe3
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2024-03-26 11:06:36 +0100

    Add test passing & test config

--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -1,31 +1,43 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideRouter(routes)],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    // component = new AppComponent();
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    // Since this is in all tests, we can pass it in the beforeEach so we don't repeat our selfs
+    // const fixture = TestBed.createComponent(AppComponent);
+    // const app = fixture.componentInstance;
+
+    expect(component).toBeTruthy();
   });
 
-  it(`should have the 'Demo ISDI Coders' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Demo ISDI Coders');
+  it(`should have the properties values...`, () => {
+    expect(component.title).toContain('Angular');
+    expect(component.brand).toContain('Aldana');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Demo ISDI Coders'
-    );
+  it('should render title...', () => {
+    // const element = fixture.nativeElement as HTMLElement;
+    // expect(element.querySelector('h1')?.textContent).toContain('Angular');
+
+    const debugElementH1 = fixture.debugElement.query(By.css('h1'));
+    expect(debugElementH1.nativeElement.textContent).toContain('Angular');
   });
 });
